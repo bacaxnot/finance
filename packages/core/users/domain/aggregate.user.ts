8ff2b8f131cb08a99d41a9fab897825622864add
bacaxnot <bacaxnot@icloud.com>
2025-11-30 07:14:51 -0500

    Add unit tests for User class functionality
    - Introduced comprehensive tests for the User class, covering user creation, full name retrieval, and primitive representation.
    - Validations include handling of valid names, unique user ID generation, acceptance of international characters, and error handling for invalid inputs.
    - Ensured that the toPrimitives method returns the correct structure and values, including trimmed names and date instances.

--- packages/core/users/domain/aggregate.user.ts
+++ packages/core/users/domain/aggregate.user.ts
@@ -1,13 +1,21 @@
 import { UserId } from "./value-object.user-id";
 import { PersonName } from "./value-object.person-name";
 
+export type UserPrimitives = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
 export class User {
   private constructor(
-    public readonly id: UserId,
-    public firstName: PersonName,
-    public lastName: PersonName,
-    public readonly createdAt: Date,
-    public updatedAt: Date
+    private readonly id: UserId,
+    private firstName: PersonName,
+    private lastName: PersonName,
+    private readonly createdAt: Date,
+    private updatedAt: Date
   ) {}
 
   static create(firstName: string, lastName: string): User {
@@ -22,5 +30,15 @@ export class User {
 
   getFullName(): string {
     return `${this.firstName} ${this.lastName}`;
+  }
+
+  toPrimitives(): UserPrimitives {
+    return {
+      id: this.id.toString(),
+      firstName: this.firstName.toString(),
+      lastName: this.lastName.toString(),
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
   }
 }
