899b724b4bde935ea1e5dcbc810a340d5d71aed8
raulps819 <raulps819@gmail.com>
2025-12-01 13:40:28 -0500

    Merge with rp

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -5,4 +5,4 @@ import * as schema from "./schema";
 const client = postgres(process.env.DATABASE_URL!);
 
 export const db = drizzle(client, { schema });
-export * from "./types";
+export * from "./types-mock";
