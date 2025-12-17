5b429ab88a87a909bcc06b123df95fa90ac390eb
bacaxnot <bacaxnot@icloud.com>
2025-12-16 19:27:24 -0500

    Refactor transaction imports and introduce FindTransaction class
    - Updated import paths for FindTransaction to reflect its new location in the domain layer, enhancing code organization.
    - Introduced a new FindTransaction class to encapsulate the logic for retrieving transactions, improving separation of concerns and maintainability.
    - Adjusted related application files to utilize the updated FindTransaction import, ensuring consistency across the transaction module.

--- apps/api/src/di/modules/transactions.ts
+++ apps/api/src/di/modules/transactions.ts
@@ -2,10 +2,10 @@ import { AccountRepository } from "@repo/core/accounts/domain/account-repository
 import { FindAccount } from "@repo/core/accounts/domain/find-account";
 import { CreateTransactionUseCase } from "@repo/core/transactions/application/create-transaction";
 import { DeleteTransactionUseCase } from "@repo/core/transactions/application/delete-transaction";
-import { FindTransaction } from "@repo/core/transactions/application/find-transaction";
 import { SearchTransactionsByAccount } from "@repo/core/transactions/application/search-transactions-by-account";
 import { SearchTransactionsByUser } from "@repo/core/transactions/application/search-transactions-by-user";
 import { UpdateTransactionUseCase } from "@repo/core/transactions/application/update-transaction";
+import { FindTransaction } from "@repo/core/transactions/domain/find-transaction";
 import { TransactionRepository } from "@repo/core/transactions/domain/transaction-repository";
 import { TransactionRepositoryPostgres } from "@repo/core/transactions/infrastructure/transaction-repository.postgres";
 import type { ContainerBuilder } from "diod";
